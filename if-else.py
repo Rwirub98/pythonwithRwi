9d79e413b013a5f5487e6002b4cbcf5bd0f89c2a
rwirub <rwirubs@gmain.com>
2025-06-13 11:19:55 +0530

    initial commit

--- if-else.py
+++ if-else.py
@@ -12,14 +12,28 @@
  ##nested if statements
  
  #we can use if else elif statement inside other if statement as well
-num=-23
-if(num==0):
-  print('natural number') 
-elif(num>=10):
-  print('positive')
-  if(num>10):
-    print('11-20')
-  else:('nothing')
-elif(num<=10):
-  print('1-10')
-else:('just nested number')    
+# num=2
+# if(num==0):
+#   print('natural number') 
+# elif(num>=10):
+#   print('positive')
+#   if(num>10):
+#     print('11-20')
+#   else:('nothing')
+# elif(num<=10):
+#   print('1-10')
+# else:('just nested number')    
+
+
+## exercise
+import time
+timestamp=int(time.strftime('%H'))
+print(timestamp)
+if(timestamp>=11):
+  print('goodMoring sir')
+elif(timestamp>=15):
+  print('goodAfternoon sir')
+elif(timestamp>=19):
+  print('goodEvening sir')
+else:'goodNight sir'    
+
